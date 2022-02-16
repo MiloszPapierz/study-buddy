dac6db8ed5e97e52f4b17008b43e792c35b772cd
MiloszPapierz <milosz.papierz1607@gmail.com>
2022-02-16 12:53:59 +0100

    Added searchbar and news feed to template

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 import Navigation from 'components/organisms/Navigation/Navigation'
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles'
+import { SearchBar } from 'components/organisms/SearchBar/SearchBar'
+
+const News = styled.div`
+   grid-row: 1/3;
+   grid-column: 3/3;
+   border-left: 1px solid ${({ theme }) => theme.colors.darkPurple};
+`
 
 const MainTemplate = ({ children }) => {
    return (
       <Wrapper>
          <Navigation />
+         <SearchBar />
          {children}
+         <News>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum</p>
+         </News>
       </Wrapper>
    )
 }
